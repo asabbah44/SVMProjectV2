4f1d967e82cb925ba1929c69f532d4ddd6c19b8c
asabbah44 <43098459+asabbah44@users.noreply.github.com>
2023-01-03 13:05:02 +0200

    Last update

--- Tuning.py
+++ Tuning.py
@@ -4,3 +4,8 @@ rate, param = grid.find_parameters('D:/data/Validate.txt',  '-log2c -1,1,1 -log2
 
 print(rate)
 print(param)
+
+# uses in the command line
+# python grid.py  -t 0  D:/data/Validate.txt
+# python grid.py  -t 1  D:/data/Validate.txt
+# python grid.py  -t 2  D:/data/Validate.txt
